68215bfec4e90df11b63b62ff2ab9f8b2686dffd
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-21 14:44:42 +0200

    Add express hello world

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("Hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (_, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+import * as express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (_: express.Request, res: express.Response) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
