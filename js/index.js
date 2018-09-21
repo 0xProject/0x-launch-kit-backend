017a9713d348d898c7b90a7686c9fd3e5a52c4cf
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-21 14:51:32 +0200

    Fix linter issues

--- js/index.js
+++ js/index.js
@@ -4,4 +4,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (_, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`); // tslint:disable-line:no-console
+});
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -4,4 +4,6 @@ const port = 3000;
 
 app.get("/", (_: express.Request, res: express.Response) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`); // tslint:disable-line:no-console
+});
