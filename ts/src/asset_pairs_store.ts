22dbe5ac1f9fbd3f6fc5126f6dc81e99baf5935a
Leonid Logvinov <logvinov.leon@gmail.com>
2018-09-25 18:28:14 +0200

    Remove types and import them from types

--- ts/src/asset_pairs_store.ts
+++ ts/src/asset_pairs_store.ts
@@ -1,35 +1,22 @@
-import { BigNumber } from '0x.js';
+import { AssetPairsItem } from '@0xproject/types';
 import * as _ from 'lodash';
 
-// TODO(leo) Those are defined in @0xproject/connect but not exported. Move them to some shared SRA types package and reuse.
-interface AssetPairSide {
-    minAmount: BigNumber;
-    maxAmount: BigNumber;
-    precision: number;
-    assetData: string;
-}
-
-interface AssetPair {
-    assetDataA: AssetPairSide;
-    assetDataB: AssetPairSide;
-}
-
 export class AssetPairsStore {
-    private readonly _assetPairs: AssetPair[];
-    constructor(assetPairs: AssetPair[]) {
+    private readonly _assetPairs: AssetPairsItem[];
+    constructor(assetPairs: AssetPairsItem[]) {
         this._assetPairs = assetPairs;
     }
-    public get(assetDataA?: string, assetDataB?: string): AssetPair[] {
+    public get(assetDataA?: string, assetDataB?: string): AssetPairsItem[] {
         if (_.isUndefined(assetDataA) && _.isUndefined(assetDataB)) {
             return this._assetPairs;
         } else if (!_.isUndefined(assetDataA) && !_.isUndefined(assetDataB)) {
-            const containsAssetDataAAndAssetDataB = (assetPair: AssetPair) =>
+            const containsAssetDataAAndAssetDataB = (assetPair: AssetPairsItem) =>
                 (assetPair.assetDataA.assetData === assetDataA && assetPair.assetDataB.assetData === assetDataB) ||
                 (assetPair.assetDataA.assetData === assetDataB && assetPair.assetDataB.assetData === assetDataA);
             return this._assetPairs.filter(containsAssetDataAAndAssetDataB);
         } else {
             const assetData = assetDataA || assetDataB;
-            const containsAssetData = (assetPair: AssetPair) =>
+            const containsAssetData = (assetPair: AssetPairsItem) =>
                 assetPair.assetDataA.assetData === assetData || assetPair.assetDataB.assetData === assetData;
             return this._assetPairs.filter(containsAssetData);
         }
