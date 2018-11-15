e394638e636436ac9e0134a56cb4c599aa706c16
Leonid Logvinov <logvinov.leon@gmail.com>
2018-11-15 11:41:31 +0100

    Make the node scripts work on windows

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
+exports.ZRX_DECIMALS = 18;
+exports.DEFAULT_PAGE = 0;
+exports.DEFAULT_PER_PAGE = 20;
