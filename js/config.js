09e3dd2ce78b1cf1fe9a44e8fd147d8a265cb11a
Jacob Evans <jacob@dekz.net>
2018-12-06 15:47:29 +1100

    Allow some config to be set in ENV

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
+// tslint:disable:custom-no-magic-numbers
 const _0x_js_1 = require('0x.js');
+const _ = require('lodash');
 // Network port to listen on
-exports.HTTP_PORT = 3000;
+exports.HTTP_PORT = process.env.HTTP_PORT || 3000;
 // A time window after which the order is considered permanently expired
 exports.ORDER_SHADOWING_MARGIN_MS = 100 * 1000; // tslint:disable-line custom-no-magic-numbers
 // Frequency of checks for permanently expired orders
@@ -10,7 +12,7 @@ exports.PERMANENT_CLEANUP_INTERVAL_MS = 10 * 1000; // tslint:disable-line custom
 // Max number of entities per page
 exports.MAX_PER_PAGE = 1000;
 // Default network id to use when not specified
-exports.NETWORK_ID = 42;
+exports.NETWORK_ID = !_.isUndefined(process.env.NETWORK_ID) ? parseInt(process.env.NETWORK_ID, 10) : 42;
 // An array of fee recipients
 exports.FEE_RECIPIENT = '0x0000000000000000000000000000000000000000';
 // A flat fee in ZRX that should be charged to the order maker
@@ -23,6 +25,6 @@ exports.WHITELISTED_TOKENS = [
     '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
 ];
 // Ethereum RPC url
-exports.RPC_URL = 'https://kovan.infura.io/v3';
+exports.RPC_URL = process.env.RPC_URL || 'https://kovan.infura.io/v3';
 // Default ERC20 token precision
 exports.DEFAULT_ERC20_TOKEN_PRECISION = 18;
